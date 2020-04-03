7f17c31b72355ddbf1938dd6218e49e4b62e5d7a
bran22 <8986312+bran22@users.noreply.github.com>
2020-04-02 18:07:34 -0700

    added carpool interface

--- src/app/join-carpool/join-carpool.component.ts
+++ src/app/join-carpool/join-carpool.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ICarpool } from '../_shared/interfaces/carpool';
 
 @Component({
   selector: 'app-join-carpool',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JoinCarpoolComponent implements OnInit {
 
-  constructor() { }
+  carpools: Observable<ICarpool[]>;
+
+  constructor(
+    private db: AngularFirestore,
+  ) {
+    this.carpools = db.collection<ICarpool>('carpools').valueChanges();
+  }
 
   ngOnInit() {
+    this.carpools.subscribe( res => console.log(res));
   }
 
 }

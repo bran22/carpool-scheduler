d2a9abf76df69ac9c57871f75cf77ab088d1140b
bran22 <8986312+bran22@users.noreply.github.com>
2020-04-08 21:24:34 -0700

    fixed absolute positioning with mapboxgl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import {ToastModule} from 'primeng/toast';
 import {MessageService} from 'primeng/api';
 import {SelectButtonModule} from 'primeng/selectbutton';
 import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import {FieldsetModule} from 'primeng/fieldset';
 
 // authored components
 import { AppRoutingModule } from './app-routing.module';
@@ -67,7 +68,8 @@ import { ViewCarpoolDetailsComponent } from './view-carpool-details/view-carpool
     OverlayPanelModule,
     ToastModule,
     SelectButtonModule,
-    ProgressSpinnerModule
+    ProgressSpinnerModule,
+    FieldsetModule
   ],
   providers: [MessageService],
   bootstrap: [AppComponent]
